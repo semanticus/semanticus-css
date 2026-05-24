fa8d67ec8010c7e0ca7b71a2d4b0a66c62af8740
Joao Goncalves <goncalves.joao@gmail.com>
2026-05-24 13:05:33 +0900

    attempt at fixing playwright tests on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  snapshotPathTemplate: '{testDir}/{testFileDir}/{testFileName}-snapshots/{arg}-{projectName}{ext}',
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: baseUrl,
