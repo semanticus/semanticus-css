05338bbe2f512f18964cc26f657d6cc5934355fc
Joao Goncalves <goncalves.joao@gmail.com>
2026-05-25 20:01:54 +0900

    minor update

--- tests/composites/alerts-demo.spec.ts
+++ tests/composites/alerts-demo.spec.ts
@@ -4,11 +4,20 @@ test.use({ viewport: { width: 1440, height: 900 } });
 
 test('/composites/alerts.demo - interaction', async ({ page }) => {
   await page.goto('/composites/alerts.demo/overview');
-  const hiddenAlert = page.locator('.info.subtle');
+  let hiddenAlert = page.locator('.info.subtle');
 
   await expect(hiddenAlert).not.toBeVisible();
 
   hiddenAlert.evaluate(el => el.textContent = 'Hello World!');
+
+  await expect(hiddenAlert).toBeVisible();
+
+  await page.goto('/composites/alerts.demo/withIconAndCloseButton');
+  hiddenAlert = page.locator('[role="status"]');
+
+  await expect(hiddenAlert).not.toBeVisible();
+
+  hiddenAlert.locator('p').evaluate(el => el.textContent = 'Hello World!');
 
   await expect(hiddenAlert).toBeVisible();
 });
