4eb61e1f774889d38c7b38821004fb8813991a88
Joao Goncalves <goncalves.joao@gmail.com>
2026-06-03 10:52:14 +0900

    Update SizesBuilder.vue

--- docs/.vitepress/theme/SizesBuilder.vue
+++ docs/.vitepress/theme/SizesBuilder.vue
@@ -425,7 +425,7 @@ const variableGroups = [
     label: "Group",
     vars: [
       {
-        name: "--group-button-spacing-horizontal",
+        name: "--group-buttons-padding-inline",
         label: "Group Button Spacing",
         desc: "Horizontal padding for buttons inside groups.",
         default: "1rem",
@@ -436,20 +436,14 @@ const variableGroups = [
     label: "Buttons",
     vars: [
       {
-        name: "--buttons-font-size",
-        label: "Button Font Size",
-        desc: "Font size for buttons.",
-        default: "1rem",
-      },
-      {
-        name: "--buttons-spacing-vertical",
-        label: "Button Spacing Vertical",
+        name: "--buttons-padding-block",
+        label: "Button Padding Block",
         desc: "Vertical padding inside buttons.",
         default: "var(--inputs-spacing-vertical)",
       },
       {
-        name: "--buttons-spacing-horizontal",
-        label: "Button Spacing Horizontal",
+        name: "--buttons-padding-inline",
+        label: "Button Padding Inline",
         desc: "Horizontal padding inside buttons.",
         default: "var(--inputs-spacing-horizontal)",
       },
